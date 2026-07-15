681fafccdeb4e18f00bc1f80b2686c56b91db7b9
designforme2024-tech <designforme2024@gmail.com>
2026-07-15 16:54:00 +0530

    Update Ads.js

--- src/pages/Ads.js
+++ src/pages/Ads.js
@@ -1,1 +1,19 @@
+import Navbar from '../components/Navbar/Navbar';
+import About from '../components/About/About';
+import Footer from '../components/Footer/Footer';
+import WhatsAppFloat from '../components/UI/WhatsAppFloat';
 
+export default function Ads() {
+  return (
+    <>
+      <Navbar />
+      <WhatsAppFloat />
+
+      <main>
+        <About />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
