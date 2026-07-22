5680769c568f32b42ae9c8fff68eee1cd11e8553
designforme2024-tech <designforme2024@gmail.com>
2026-07-22 18:54:26 +0530

    Update script.js

--- public/Landingpage-biz/script.js
+++ public/Landingpage-biz/script.js
@@ -187,6 +187,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (response.ok) {
           console.log('Lead submitted to Formspree successfully.');
+
+  gtag_report_conversion(function () {
+
+        form.reset();
+
+        successModal.classList.add("active");
+
+        console.log('tracked to Google Ads');
+
+    });
+          
           form.reset();
           if (successModal) {
             successModal.classList.add('open');
